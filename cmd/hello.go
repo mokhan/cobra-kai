1185505c08bbff425211bf4c69ee8b47ef3a333f
mo khan <mo.khan@gmail.com>
2020-10-15 15:47:48 -0600

    feat: add nested say hello command

--- cmd/hello.go
+++ cmd/hello.go
@@ -21,9 +21,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// startCmd represents the start command
-var startCmd = &cobra.Command{
-	Use:   "start",
+// helloCmd represents the hello command
+var helloCmd = &cobra.Command{
+	Use:   "hello",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -32,20 +32,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello. Let's get started!")
+		fmt.Println("Hello, world!")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(startCmd)
+	sayCmd.AddCommand(helloCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// helloCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// helloCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/say.go
+++ cmd/say.go
@@ -16,14 +16,14 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
+	"errors"
 
 	"github.com/spf13/cobra"
 )
 
-// startCmd represents the start command
-var startCmd = &cobra.Command{
-	Use:   "start",
+// sayCmd represents the say command
+var sayCmd = &cobra.Command{
+	Use:   "say",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -31,21 +31,21 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello. Let's get started!")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return errors.New("What should I say?")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(startCmd)
+	rootCmd.AddCommand(sayCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// sayCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// sayCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
